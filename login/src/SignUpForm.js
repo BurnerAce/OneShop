6fec2e43a83d5d9b37287022812e717cb64ed82d
BurnerAce <hanzalasharique04@gmail.com>
2024-06-14 23:34:21 +0530

    some more

--- login/src/SignUpForm.js
+++ login/src/SignUpForm.js
@@ -22,7 +22,7 @@ const SignupForm = () => {
 
         console.log('Submitting form...');
         try {
-            const res = await axios.post('http://localhost:5000/signup', formData);
+            const res = await axios.post('https://one-shop-tsvt.vercel.app/signup', formData);
             console.log('Response:', res.data);
             navigate('/clogin'); // Redirect to login page on successful signup
         } catch (err) {
