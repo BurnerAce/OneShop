63c37b5e08ef333d3ac7c579760ab0704cea5c33
BurnerAce <hanzalasharique04@gmail.com>
2024-06-15 15:40:43 +0530

    f

--- login/src/Home.js
+++ login/src/Home.js
@@ -6,7 +6,6 @@ const HomePage = () => {
         <div className='homepage'>
             <div>
                 <h1 style={{fontSize:"6rem", margin:"1rem"}}>Welcome to OneShop!</h1>
-                <br></br>
                 <p style={{textAlign:"center" , fontSize:"2rem"}}>Are you a customer or an owner?</p>
                 <div style={{position:"relative", top:"-1.5rem"}}>
                     <Link to="/csignup">
